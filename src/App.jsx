35e4b593daff29e56d0391f922ce3fdacd3a3646
Renan Augusto dos Santos <devrsantos@gmail.com>
2023-02-14 21:30:34 -0300

    Aula 23 Concluída

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,53 @@ import { Post } from './components/Post';
 import './global.css';
 import styles from "./App.module.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/2254731?v=4',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('02/08/2023 16:45:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/6643122?v=4',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'}
+    ],
+    publishedAt: new Date('02/09/2023 22:02:00')
+  },
+];
 
 const App = () => {
   return (
     <div>
       <Header />
-
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {
+            posts.map(post => {
+              return <Post 
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            })
+          }
         </main>
       </div>
     </div>
--- src/Post.jsx
+++
@@ -1,3 +0,0 @@
-export function Post({author, content}) {
-    return <p></p>
-}
